bcd67758ecfb20585594ee3786d9a9c5e483d417
Sarah <spfohman@email.essex.edu>
2021-08-17 09:54:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+function appendCat(name){
+    var newCats = [...cats, "Broom"];
+    return newCats;
+}
+function prependCat(name){
+    var moreCats = [name, ...cats];
+    return moreCats;
+}
+function removeLastCat(name){
+    var lessCats = [...cats];
+    lessCats.pop();
+    return lessCats;
+}
+function removeFirstCat(name){
+    var moreCats = [...cats];
+    moreCats.shift();
+    return moreCats;
+}
